fde34c42ee46dd61cdf61d433a7a1f0a8764900e
TIMEUS ryan <timeusryan@gmail.com>
2025-01-07 17:22:08 +0100

    Added finite difference method for European options

--- src/contract.py
+++ src/contract.py
@@ -2,65 +2,119 @@ import numpy as np
 from abc import ABC, abstractmethod
 
 
+# ======================================================================================================================
+# Contracts
+# ======================================================================================================================
+
+
 class Contract(ABC):
     def __init__(self, maturity: float) -> None:
         self.maturity = maturity
 
     @abstractmethod
-    def payoff(self, paths: np.ndarray) -> np.ndarray:
+    def payoff(self, paths: np.ndarray, all_paths: bool) -> np.ndarray:
         pass
 
     @abstractmethod
-    def type(self) -> str:
+    def name(self) -> str:
         pass
 
 
-class EuropeanCall(Contract):
-    def __init__(self, maturity: float, strike: float) -> None:
+# ======================================================================================================================
+# European Contracts
+# ======================================================================================================================
+
+
+class EuropeanContract(Contract):
+    def __init__(self, maturity: float, strike: float):
         super().__init__(maturity)
         self.strike = strike
 
-    def payoff(self, paths: np.ndarray) -> np.ndarray:
-        return np.maximum(paths[:, -1] - self.strike, 0.0)
 
-    def type(self) -> str:
+# ----------------------------------------------------------------------------------------------------------------------
+# European Call
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+class EuropeanCall(EuropeanContract):
+    def __init__(self, maturity: float, strike: float) -> None:
+        super().__init__(maturity, strike)
+
+    def payoff(self, paths: np.ndarray, all_paths: bool = True) -> np.ndarray:
+        if all_paths:
+            return np.maximum(paths[:, -1] - self.strike, 0.0)
+        else:
+            return np.maximum(paths - self.strike, 0.0)
+
+    def name(self) -> str:
         return "European Call"
 
 
-class EuropeanPut(Contract):
+# ----------------------------------------------------------------------------------------------------------------------
+# European Put
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+class EuropeanPut(EuropeanContract):
     def __init__(self, maturity: float, strike: float) -> None:
-        super().__init__(maturity)
-        self.strike = strike
+        super().__init__(maturity, strike)
 
-    def payoff(self, paths: np.ndarray) -> np.ndarray:
-        return np.maximum(self.strike - paths[:, -1], 0.0)
+    def payoff(self, paths: np.ndarray, all_paths: bool = True) -> np.ndarray:
+        if all_paths:
+            return np.maximum(self.strike - paths[:, -1], 0.0)
+        else:
+            return np.maximum(self.strike - paths, 0.0)
 
-    def type(self) -> str:
+    def name(self) -> str:
         return "European Put"
 
 
-class AsianCall(Contract):
-    def __init__(self, maturity: float, strike: float) -> None:
+# ======================================================================================================================
+# Asian Contracts
+# ======================================================================================================================
+
+
+class AsianContract(Contract):
+    def __init__(self, maturity: float, strike: float):
         super().__init__(maturity)
         self.strike = strike
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Asian Call
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+class AsianCall(AsianContract):
+    def __init__(self, maturity: float, strike: float) -> None:
+        super().__init__(maturity, strike)
 
     def payoff(self, paths: np.ndarray) -> np.ndarray:
         return np.maximum(np.mean(paths, axis=1) - self.strike, 0)
 
-    def type(self) -> str:
+    def name(self) -> str:
         return "Asian Call"
 
 
-class AsianPut(Contract):
+# ----------------------------------------------------------------------------------------------------------------------
+# Asian Put
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+class AsianPut(AsianContract):
     def __init__(self, maturity: float, strike: float) -> None:
-        super().__init__(maturity)
-        self.strike = strike
+        super().__init__(maturity, strike)
 
     def payoff(self, paths: np.ndarray) -> np.ndarray:
         return np.maximum(self.strike - np.mean(paths, axis=1), 0)
 
-    def type(self) -> str:
+    def name(self) -> str:
         return "Asian Put"
+
+
+# ======================================================================================================================
+# Lookback Contract
+# ======================================================================================================================
 
 
 class Lookback(Contract):
@@ -74,8 +128,13 @@ class Lookback(Contract):
         correction = 1.0 - beta * self.vol * np.sqrt(dt)
         return paths[:, -1] - np.min(paths, axis=1) * correction
 
-    def type(self) -> str:
+    def name(self) -> str:
         return "Lookback"
+
+
+# ======================================================================================================================
+# Digital Contract
+# ======================================================================================================================
 
 
 class Digital(Contract):
@@ -85,5 +144,5 @@ class Digital(Contract):
     def payoff(self, paths: np.ndarray) -> np.ndarray:
         return np.heaviside(paths[:, -1] - 1, 0)
 
-    def type(self) -> str:
+    def name(self) -> str:
         return "Digital"
--- src/fdm.py
+++ src/fdm.py
@@ -1,1 +1,76 @@
-# TODO: Implement finite difference method for solving PDEs
+import numpy as np
+
+from contract import EuropeanContract, AsianContract
+from model import Model
+
+
+# ======================================================================================================================
+# FDM
+# ======================================================================================================================
+
+
+class FDM:
+    def __init__(self, l: int, m: int):
+        self.l = l
+        self.m = m
+
+
+# ======================================================================================================================
+# European FDM
+# ======================================================================================================================
+
+
+class EuropeanFDM(FDM):
+    def __init__(self, l: int, m: int) -> None:
+        super().__init__(l, m)
+
+    def solve(
+        self,
+        model: Model,
+        contract: EuropeanContract,
+        xmin: float,
+        xmax: float,
+        boundary_min: float | np.ndarray = 0.0,
+        boundary_max: float | np.ndarray = 0.0,
+        theta: float = 0.5,
+    ) -> tuple[np.ndarray, np.ndarray]:
+        dx = (xmax - xmin) / (self.l + 1)
+        dt = contract.maturity / self.m
+        x = np.linspace(xmin, xmax, self.l + 2)
+
+        d1 = model.diffusion(0, x[2:-1]) ** 2 / (2 * dx**2) - model.drift(0, x[2:-1]) / (2 * dx)
+        d2 = -(model.interest_rate + model.diffusion(0, x[1:-1]) ** 2 / dx**2)
+        d3 = model.diffusion(0, x[1:-2]) ** 2 / (2 * dx**2) + model.drift(0, x[1:-2]) / (2 * dx)
+        a = np.diag(d1 * np.ones(self.l - 1), -1) + np.diag(d2 * np.ones(self.l)) + np.diag(d3 * np.ones(self.l - 1), 1)
+
+        f = np.zeros((self.l, self.m))
+        f[0], f[-1] = boundary_min, boundary_max
+
+        i = np.eye(self.l)
+        c1 = i - theta * dt * a
+        c2 = i + (1 - theta) * dt * a
+
+        u = contract.payoff(x, all_paths=False)
+        for i in range(self.m):
+            u[1:-1] = np.dot(np.linalg.inv(c1), np.dot(c2, u[1:-1]) + dt * f[:, i])
+
+        return x, u
+
+
+# ======================================================================================================================
+# Asian FDM
+# ======================================================================================================================
+
+
+class AsianFDM(FDM):
+    def __init__(self, l: int, m: int) -> None:
+        super().__init__(l, m)
+
+    def solve(
+        self, model: Model, contract: AsianContract, xmin: float, xmax: float, theta: float = 0.5
+    ) -> tuple[np.ndarray, np.ndarray]:
+        dx = (xmax - xmin) / (self.l + 1)
+        dt = contract.maturity / self.m
+        x = np.linspace(xmin, xmax, self.l + 2)
+
+        return None
